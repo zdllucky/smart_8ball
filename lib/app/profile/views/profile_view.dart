1a8ae0e69524acb3b1b0bcf1d1d8ca0d3b7962e3
zdllucky <animereader@yandex.kz>
2023-03-29 01:23:28 +0600

    Encapsulated profile anonimous view section

--- lib/app/profile/views/profile_view.dart
+++ lib/app/profile/views/profile_view.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import '../../shared/widgets/themed_c_s_navigation_bar.dart';
+import '../widgets/anonymous_block.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
@@ -56,56 +54,7 @@ class _ProfileViewState extends State<ProfileView> {
             ),
             SliverFillRemaining(
               hasScrollBody: false,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(32),
-                    child: Center(
-                      child: Text(
-                        'Sign up or sign in \nto upgrade your account \nand get even more \ntokens and features!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontFamily:
-                                GoogleFonts.averiaSerifLibre().fontFamily,
-                            color: const Color(0xffD4C6BD),
-                            letterSpacing: .3),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(32),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        CupertinoButton(
-                            borderRadius: BorderRadius.circular(100),
-                            padding: const EdgeInsets.symmetric(horizontal: 32),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Icon(FluentIcons.arrow_enter_20_regular),
-                                SizedBox(width: 4),
-                                Text('Sign in'),
-                              ],
-                            ),
-                            onPressed: () => context.push('/sign-in')),
-                        CupertinoButton.filled(
-                            borderRadius: BorderRadius.circular(100),
-                            padding: const EdgeInsets.symmetric(horizontal: 32),
-                            onPressed: () => context.push('/sign-up'),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Icon(FluentIcons.person_add_20_regular),
-                                SizedBox(width: 4),
-                                Text('Sign up'),
-                              ],
-                            )),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+              child: _user!.isAnonymous ? const AnonymousBlock() : null,
             )
           ],
         ),
---
+++ lib/app/profile/widgets/anonymous_block.dart
@@ -0,0 +1,55 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../shared/widgets/rounded_c_button.dart';
+
+class AnonymousBlock extends StatelessWidget {
+  const AnonymousBlock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(32),
+          child: Center(
+            child: Text(
+              'Sign up or sign in \nto upgrade your account \nand get even more \ntokens and features!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontFamily: GoogleFonts.averiaSerifLibre().fontFamily,
+                  color: const Color(0xffD4C6BD),
+                  letterSpacing: .3),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: RoundedCButton(
+                  filled: false,
+                  icon: FluentIcons.arrow_enter_20_regular,
+                  text: 'Sign in',
+                  onPressed: () => context.push('/sign-in'),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: RoundedCButton(
+                  icon: FluentIcons.person_add_20_regular,
+                  text: 'Sign up',
+                  onPressed: () => context.push('/sign-up'),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
