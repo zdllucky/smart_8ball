5585a31ec10921df8347206e693323b79b81f721
zdllucky <animereader@yandex.kz>
2023-04-09 18:24:00 +0600

    Updated form widget to allow more support

--- lib/_widgets/common/widgets/f_builder_c_t_field.dart
+++ lib/_widgets/common/widgets/f_builder_c_t_field.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class FBuilderCTField extends StatelessWidget {
-  const FBuilderCTField(
-      {super.key,
-      this.validator,
-      required this.name,
-      this.placeholder,
-      this.obscureText = false,
-      this.decoration,
-      this.minLines,
-      this.maxLines});
+class FBuilderCTField extends StatefulWidget {
+  const FBuilderCTField({
+    super.key,
+    this.validator,
+    required this.name,
+    this.placeholder,
+    this.obscureText = false,
+    this.decoration,
+    this.minLines,
+    this.maxLines,
+    this.autofocus = false,
+    this.unFocusOnSwipe = false,
+  });
   final String? Function(String?)? validator;
   final String name;
   final String? placeholder;
@@ -18,15 +21,49 @@ class FBuilderCTField extends StatelessWidget {
   final BoxDecoration? decoration;
   final int? minLines;
   final int? maxLines;
+  final bool autofocus;
+  final bool unFocusOnSwipe;
+
+  @override
+  State<FBuilderCTField> createState() => _FBuilderCTFieldState();
+}
+
+class _FBuilderCTFieldState extends State<FBuilderCTField> {
+  late FocusNode _focusNode;
+  bool _isFocusedInitially = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FormBuilderField(
-      name: name,
-      validator: validator,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      builder: (FormFieldState<String?> field) {
-        return CupertinoFormRow(
+    if (widget.autofocus && !_isFocusedInitially) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        FocusScope.of(context).requestFocus(_focusNode);
+        setState(() => _isFocusedInitially = true);
+      });
+    }
+
+    return Listener(
+      onPointerMove: widget.unFocusOnSwipe
+          ? (e) {
+              if (e.down) FocusScope.of(context).unfocus();
+            }
+          : null,
+      child: FormBuilderField(
+        name: widget.name,
+        validator: widget.validator,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        builder: (FormFieldState<String?> field) => CupertinoFormRow(
           padding: const EdgeInsets.symmetric(vertical: 6),
           error: field.errorText != null
               ? Padding(
@@ -34,22 +71,24 @@ class FBuilderCTField extends StatelessWidget {
                   child: Text(field.errorText!),
                 )
               : null,
-          child: decoration != null
+          child: widget.decoration != null
               ? CupertinoTextField(
-                  placeholder: placeholder,
-                  obscureText: obscureText,
-                  minLines: minLines,
-                  maxLines: maxLines,
-                  decoration: decoration,
+                  focusNode: _focusNode,
+                  placeholder: widget.placeholder,
+                  obscureText: widget.obscureText,
+                  minLines: widget.minLines,
+                  maxLines: widget.maxLines,
+                  decoration: widget.decoration,
                   onChanged: (value) => field.didChange(value))
               : CupertinoTextField(
-                  placeholder: placeholder,
-                  minLines: minLines,
-                  maxLines: maxLines,
-                  obscureText: obscureText,
+                  focusNode: _focusNode,
+                  placeholder: widget.placeholder,
+                  minLines: widget.minLines,
+                  maxLines: widget.maxLines,
+                  obscureText: widget.obscureText,
                   onChanged: (value) => field.didChange(value)),
-        );
-      },
+        ),
+      ),
     );
   }
 }
