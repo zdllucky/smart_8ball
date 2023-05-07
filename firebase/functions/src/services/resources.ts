cb79ca66ae719148da914e178d81e963c2d71e6f
zdllucky <animereader@yandex.kz>
2023-05-07 07:38:40 +0600

    Implemented resources service

---
+++ firebase/functions/src/services/resources.ts
@@ -0,0 +1,33 @@
+import { createHttpError } from "express-zod-api";
+import repositories from "../repositories";
+import _admin from "./admin";
+
+const resourcesService = ({
+  triesRepo,
+  admin,
+}: {
+  triesRepo: typeof repositories.triesAvailable;
+  admin: typeof _admin;
+}) => {
+  const addBasicTriesAmount = async (
+    userOrDeviceId: string,
+    amount: number,
+    { allowNegative = false }: { allowNegative?: boolean } = {}
+  ) => {
+    await admin.firestore().runTransaction(async (transaction) => {
+      const doc = triesRepo.reference.doc(userOrDeviceId);
+      const { basicTries } = await triesRepo.getResources(userOrDeviceId);
+
+      if (basicTries + amount < 0 && !allowNegative)
+        throw createHttpError(400, "No tries available");
+
+      transaction.update(doc, {
+        "resources.basicTries": basicTries + amount,
+      });
+    });
+  };
+
+  return { addBasicTriesAmount };
+};
+
+export default resourcesService;
